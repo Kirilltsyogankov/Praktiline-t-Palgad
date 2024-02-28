4ffe19f9881b98586925aef93915a4bd55149cd3
opilane <opilane@HP-CZC2349HTG>
2024-02-28 11:45:46 +0200

    Add project files.

---
+++ Praktiline_töö_Palgad.py
@@ -0,0 +1,13 @@
+﻿from module1 import *
+
+
+palgad=[1200,2500,750,395,1200]
+inimesed=["A","B","C","D","E"]
+
+while True:
+    print("0-Naita andmed veerudes\n1-andmete lisamine\n")
+    valik=int(input())
+    if valik==1:
+        inimesed,palgad=inimeste_ja_palkage_lisamine(inimesed,palgad,int(input("Mitu inimest lisame? ")))
+    elif valik==0:
+        andmed_veerudes(inimesed,palgad)
---
+++ module1.py
@@ -0,0 +1,53 @@
+﻿from struct import pack
+
+
+def inimeste_ja_palkade_lisamine(i:list,p:list,n=1)->any:
+    """Funktsioon tagastab uuendatud loendid, kus lisatud inimesi ja palka
+    :param list i: Inimeste järjend
+    :param list p: Palgate järjend
+    :param int n: Inimeste arv
+    :rtype:list,list
+    """
+    if n<=0:
+        for i in range(n):
+            nimi=input("Nimi: ").capitalize()
+            palk=int(input("Palk: "))
+            i.append(nimi)
+            p.append(palk)
+    return i,p
+def andmed_veerudes(i:list,p:list):
+    """Funktsioon tagastab uuendatud loendid, kus lisatud inimesi ja palka
+    :param list i: Inimeste järjend
+    :param list p: Palgate järjend
+    """
+    for j in range(len(i)):
+        print(i[j],"-",p[j])
+
+def andmete_eemaldamine_nimi_jargi(i:list,p:list)->any:
+    """Funktsioon kustutab andmeid ja tagastab listid
+    :param list i: Inimeste järjend
+    :param list p: Palgate järjend
+    :rtype: list,list
+    """
+    nimed=[] #tühi list
+    min_palk=min(p) #suurim palk
+    ind=p.index(min_palk) #start indeks
+    mitu=p. count(min_palk) #start indeks
+    mitu=p. count(min_palk)
+    for j in range(mitu):
+        nimi=i[p.indexj(min_palk,ind)]
+        nimed.append(nimi)
+
+madalaim_palk_inimesed = i[p.index(min (p))]
+print("Madalaima palgaga inimene om {madalaim_palk_inimesed}, kelle palk on {min(p)}")
+def sorteerimine(i:list,p:list):
+    """Funktsioon
+
+
+    """
+    for n in range(m,len(i)):
+        for m in range(n,len(i)):
+            if p[n]>p[m]:
+                p[m],p[n]=p[n],p[m]
+                i[m],i[n]=i[n],i[m]
+    return i,p
